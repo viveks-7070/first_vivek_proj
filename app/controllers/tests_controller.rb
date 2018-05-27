c1d4764e9b44520594c4d785300996351784d04e
vivek <viveks7070@gmail.com>
2018-05-27 23:13:17 +0530

    sdf

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,25 @@
 class TestsController < ApplicationController
   def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = test.new(test_params)
+    if @test.valid?
+      @test.save
+      redirect_to tests_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:tests).permit(:name)
   end
 end
