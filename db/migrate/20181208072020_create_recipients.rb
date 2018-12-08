23fb3953b8f89f60082c308b459b24234faef314
vivek <viveks7070@gmail.com>
2018-12-08 12:54:16 +0530

    login commit

---
+++ db/migrate/20181208072020_create_recipients.rb
@@ -0,0 +1,9 @@
+class CreateRecipients < ActiveRecord::Migration
+  def change
+    create_table :recipients do |t|
+      t.integer :user_id
+      t.integer :message_id
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603151302) do
+ActiveRecord::Schema.define(version: 20181208072020) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id",  limit: 4
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "recipients", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "message_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "samples", force: :cascade do |t|
     t.string   "sample_name", limit: 255
@@ -23,6 +37,15 @@ ActiveRecord::Schema.define(version: 20180603151302) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.string   "email",              limit: 255
+    t.string   "encrypted_password", limit: 255
+    t.string   "salt",               limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
 end
